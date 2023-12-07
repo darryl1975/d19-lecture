12878483e2e3334dc6315873ccf6d19df28caf82
darryl1975 <darryl1975@hotmail.com>
2023-12-07 09:18:08 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/sg/d19lecture/D19LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.sg.d19lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D19LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D19LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/sg/d19lecture/D19LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.sg.d19lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D19LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
