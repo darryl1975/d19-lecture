6f82176b42d6d483d3e237629e476f8e88847e80
darryl1975 <darryl1975@hotmail.com>
2023-12-07 09:39:47 +0800

    added commandlinerunner

--- src/main/java/sg/edu/nus/sg/d19lecture/D19LectureApplication.java
+++ src/main/java/sg/edu/nus/sg/d19lecture/D19LectureApplication.java
@@ -1,13 +1,36 @@
 package sg.edu.nus.sg.d19lecture;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class D19LectureApplication {
+public class D19LectureApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D19LectureApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		String pathFileName = "c:\\data\\employees.json"; // for windows users
+		pathFileName = "//data";
+
+		File file = new File(pathFileName);
+		InputStream is = new FileInputStream(file);
+
+		StringBuilder resultStringBuilder = new StringBuilder();
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+			
+		}
 	}
 
 }
